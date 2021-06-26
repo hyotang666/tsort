db26593220288093185f28d27dc8adedb0c53f9f
SATO Shinichi <hyotang.karakoma@gmail.com>
2021-06-27 08:40:03 +0900

    Cleanup: Integrate files.

--- tsort.asd
+++ tsort.asd
@@ -8,11 +8,7 @@
    "named-readtables" ; Manage readtables.
    "trestrul" ; Utilities for TREe STRUctured List.
    )
-  :components((:file "package")
-              (:file "type" :depends-on ("package"))
-              (:file "report-error" :depends-on("package"))
-              (:file "tsort" :depends-on("report-error"))
-              ))
+  :components((:file "tsort")))
 ;; Perform method below is added by JINGOH.GENERATOR.
 (defmethod component-depends-on((o test-op) (c (eql (find-system "tsort"))))
   (append (call-next-method)'((test-op "tsort.test"))))
--- tsort.lisp
+++ tsort.lisp
@@ -1,4 +1,103 @@
+(in-package :cl-user)
+
+(defpackage :tsort
+  (:use :cl)
+  (:export ;;;; main api
+           #:tsort
+           ;;;; conditions
+           ;; super condition
+           #:graph-error
+           ;; sub conditions
+           #:recursive-graph
+           #:trampoline-graph
+           #:uncomplete-graph
+           #:invalid-graph))
+
 (in-package :tsort)
+
+;;;; TYPES
+;;; Actually these are not needed, but for explaining implementation.
+
+(deftype graph () "Graph represented by list." 'nodes)
+
+(deftype node ()
+  "Element of GRAPH, represented by list."
+  '(cons subject directions))
+
+(deftype nodes () "List which contains only node." 'list)
+
+(deftype subject ()
+  "Identifier for NODE. We can say this is node's name, roughly."
+  '(not null))
+
+(deftype subjects () "List which contains subject only." 'list)
+
+(deftype direction () 'subjects)
+
+(deftype directions () "List which contains direction only." 'list)
+
+;;;; CONDITIONS
+
+(define-condition graph-error (simple-error) ())
+
+(define-condition recursive-graph (graph-error)
+  ()
+  (:report
+   (lambda (c *standard-output*)
+     (format t "~S: Recursive graph: ~S" 'tsort
+             (car (simple-condition-format-arguments c))))))
+
+(define-condition trampoline-graph (graph-error)
+  ()
+  (:report
+   (lambda (c *standard-output*)
+     (apply #'format t "~S: Trampoline graph.~%Especially ~S and ~S." 'tsort
+            (simple-condition-format-arguments c)))))
+
+(define-condition uncomplete-graph (graph-error)
+  ()
+  (:report
+   (lambda (c *standard-output*)
+     (format t "~S: Uncomplete graph.~%Especially lack of ~S." 'tsort
+             (simple-condition-format-arguments c)))))
+
+(define-condition invalid-graph (graph-error)
+  ()
+  (:report
+   (lambda (c *standard-output*)
+     (format t "~S: Each node must proper-list, but ~S." 'tsort
+             (simple-condition-format-arguments c)))))
+
+(defun report-error (rest &key (test #'eql) (key #'identity))
+  (let ((arg (circler-graph-p rest :test test :key key)))
+    (if arg
+        (if (eq (first arg) (second arg)) ; it is self recursive.
+            (error 'recursive-graph :format-arguments arg)
+            (error 'trampoline-graph :format-arguments arg))
+        (error 'uncomplete-graph :format-arguments (lack rest)))))
+
+(defun circler-graph-p (rest &key (test #'eql) (key #'identity))
+  (loop :for node1 :in rest
+        :for (subject1 . direction1) = node1
+        :if (find subject1 rest
+                  :test (lambda (subject1 node)
+                          (destructuring-bind
+                              (subject2 . direction2)
+                              node
+                            (and (find subject1 direction2 :test test :key key)
+                                 (find subject2 direction1
+                                       :test test
+                                       :key key)))))
+          :collect it :into node2
+          :and :do (return (cons node1 node2))))
+
+(defun lack (nodes)
+  (loop :for (subject . direction) :in nodes
+        :collect subject :into subjects
+        :append direction :into directions
+        :finally (return (nset-difference directions subjects))))
+
+;;;; TSORT
 
 (defun tsort (graph &key (test #'eql) (key #'identity) group)
   (let ((engrouper
